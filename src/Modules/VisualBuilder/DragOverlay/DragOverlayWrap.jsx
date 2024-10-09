0c670121e010a0f755b88c9fc37048e700a24cf0
System Administrator <root@Shazans-MacBook-Air.local>
2024-10-10 00:46:23 +0530

    UI changes: Part3

--- src/Modules/VisualBuilder/DragOverlay/DragOverlayWrap.jsx
+++ src/Modules/VisualBuilder/DragOverlay/DragOverlayWrap.jsx
@@ -1,27 +1,27 @@
 /* eslint-disable react/prop-types */
-import React, { useState, useEffect } from 'react'
-import { DragOverlay  } from '@dnd-kit/core'
+import React, { useState, useEffect } from 'react';
+import { DragOverlay } from '@dnd-kit/core';
 import { TrayElementButtonOverlay } from '../RightSideTray/CollapsedTray/ComponentTray/TrayBtn/TrayElementButton';
 
-export default function DragOverlayWrap({draggedItem}) {
-  const [node, setNode] = useState(null)
+export default function DragOverlayWrap({ draggedItem }) {
+  const [node, setNode] = useState(null);
 
   useEffect(() => {
-    console.log("showing drag overlay");
+    console.log('showing drag overlay');
     console.log(draggedItem);
-  const isTrayElementBtn = draggedItem?.data?.current?.isTrayElement; 
+    const isTrayElementBtn = draggedItem?.data?.current?.isTrayElement;
 
-  if(isTrayElementBtn){
-    console.log("helo helo");
-    
-    setNode(<TrayElementButtonOverlay name={draggedItem?.data?.current?.name}></TrayElementButtonOverlay>)
-  }
-  }, [draggedItem])
-  
+    if (isTrayElementBtn) {
+      console.log('helo helo');
 
-  
+      setNode(
+        <TrayElementButtonOverlay
+          name={draggedItem?.data?.current?.name}
+          type={draggedItem?.data?.current?.type}
+        ></TrayElementButtonOverlay>,
+      );
+    }
+  }, [draggedItem]);
 
-  return (
-    <DragOverlay>{node}</DragOverlay>
-  )
+  return <DragOverlay>{node}</DragOverlay>;
 }
