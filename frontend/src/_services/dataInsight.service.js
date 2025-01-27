ec2e90dbca018a549ff58b6b5ca27b9546ba8b28
kunalPiggiestack <kunal@piggiestack.com>
2025-01-28 03:21:15 +0530

    Removed commments not required

--- frontend/src/_services/dataInsight.service.js
+++ frontend/src/_services/dataInsight.service.js
@@ -17,29 +17,3 @@ function getInsight(body) {
         handleResponse,
     );
 }
-
-// function fetchWithTimeout(url, options, timeout = 5000) {
-//     return Promise.race([
-//         fetch(url, options),
-//         new Promise((_, reject) =>
-//             setTimeout(() => reject(new Error('Request timed out')), timeout)
-//         ),
-//     ]);
-// }
-
-// // Example usage
-// function getInsight(body) {
-//     const requestPayload = {
-//         method: 'GET',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(body),
-//     };
-
-//     return fetchWithTimeout("http://localhost:8001/events/saved/", requestPayload, 10000) // 10-second timeout
-//         .then(handleResponse)
-//         .catch((error) => {
-//             console.error('Error:', error.message);
-//         });
-// }
