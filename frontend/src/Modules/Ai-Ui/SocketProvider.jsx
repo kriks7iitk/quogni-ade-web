bc9050fc266d8b3af730fd88f40b17fffd14e72c
kriks7iitk <kriks.iitk@gmail.com>
2025-02-06 17:01:08 +0530

    removed socket

--- frontend/src/Modules/Ai-Ui/SocketProvider.jsx
+++ frontend/src/Modules/Ai-Ui/SocketProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { io } from "socket.io-client";
+// import { io } from "socket.io-client";
 
 const SocketContext = createContext();
 
