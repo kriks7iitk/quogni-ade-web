89be141b9d7b06d2db71c3063e68a2c653301466
kriks7iitk <kriks.iitk@gmail.com>
2025-03-05 11:07:05 +0530

    removed server

--- server/src/modules/session/interface/sesssion.interface.ts
+++
@@ -1,7 +0,0 @@
-import { AuthType } from "@prisma/client";
-
-export type SessionCreate = {
-  ip: string;
-  userId: number;
-  authType: AuthType;
-};
--- server/src/modules/user/constants/error-codes.ts
+++
@@ -1,3 +0,0 @@
-export const USER_MOD_ERROR_CODES = {
-  USER_EXIST: "um101",
-};
